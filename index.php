d3373fe07f253c49f1bad9ef5184d97980c30ab8
AKIRONAKAHIGASHI <akizo.rain@icloud.com>
2021-12-16 23:15:27 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+
+<title>エンジニアブログの開設に向けて</title>
+
+<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>">
+<meta name="description" content="サイトの概要">
+
+
+</head>
+
+<body>
+<div class="inner">
+    <h1>いちからスタート</h1>
+</div>
+
+</body>
+</html>
+
