b795c47323a94ccb86088d7e2300d6be98c184da
AKIRONAKAHIGASHI <akizo.rain@icloud.com>
2021-12-30 00:50:19 +0900

    fix header_nav_link

---
+++ footer.php
@@ -0,0 +1,3 @@
+<div id="footer">
+    <p>&#169;2021 AKIZO BLOG ALL RIGHTS RESERVED. </p>
+</div>
---
+++ page-output.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+<div class="inner">
+    <div class="content">
+    <span class="material-icons-two-tone">build</span>
+        <h2>Output準備中</h2>
+    </div>
+</div>
+
+<?php get_footer(); ?>
+
+</body>
+</html>
