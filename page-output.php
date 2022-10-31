75f163396b106dcc5bf4919074049358bd1dde4b
AKIRONAKAHIGASHI <akizo.rain@icloud.com>
2022-11-01 01:50:24 +0900

    【WIP】output page add

--- page-output.php
+++ page-output.php
@@ -1,43 +1,39 @@
 <?php get_header(); ?>
 
-<div class="inner task_admin">
-  <div class="content">
-  </div>
+<!-- bottom navigation -->
 
-  <div id="table">
-    <div class="task_contents">
-      <div class="task_content_header todo"></div>
-      <div class="task_content_title">ToDo</div>
-      <div class="task_new">
-        <button class="task_submit" type="button">
-          <span class="material-symbols-outlined">add</span>
-          <span>追加</span>
-        </button>
-      </div>
-      <ul class="task_list">
-      </ul>
-    </div>
-    <div class="task_contents">
-      <div class="task_content_header done"></div>
-      <div class="task_content_title">Done</div>
-      <div class="task_new">
-        <button class="task_submit" type="button">
-          <span class="material-symbols-outlined">add</span>
-          <span>追加</span>
-        </button>
-      </div>
-      <ul class="task_list"></ul>
-    </div>
-  </div>
-</div>
-<div class="task_input">
-  <div class="content">
-    <textarea name="contact" rows="3"></textarea>
-    <button class="task_add" type="button">
-      OK
-    </button>
-  </div>
-</div>
+<ul class="bottom-menu">
+    <li>
+        <a href="#">
+			<i class="blogicon-home"></i><br>
+			<span class="mini-text">Home</span>
+		</a>
+    </li>
+    <li>
+        <a href="#">
+			<i class="blogicon-list"></i><br>
+			<span class="mini-text">Blog</span>
+		</a>
+    </li>
+    <li>
+		<a href="#" target="_blank">
+			<i class="blogicon-c"></i><br>
+			<span class="mini-text">Output</span>
+		</a>
+    </li>
+    <li>
+        <a href="#">
+			<i class="blogicon-d"></i><br>
+			<span class="mini-text">Pokemon</span>
+		</a>
+    </li>
+    
+</ul>
+
+<button class="flooting_btn">
+	<span class="material-symbols-outlined">add</span>
+</button>
+
 
 <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/todo.js"></script>
 </body>
